04f5ad8dadc72a36bab1bfbba9ffc0b25fc15857
brunowapstore <62388773+brunowapstore@users.noreply.github.com>
2021-10-16 15:20:54 -0300

    Mensagens de erro e cadastro
    Cadastro e mensagens de erros implementadas

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,5 +1,5 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  res.locals.errors = req.flash('errors');
   next();
 };
 
@@ -8,9 +8,11 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && 'EBADCSRFTOKEN' === err.code) {
+  if(err) {
     return res.render('404');
   }
+
+  next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
